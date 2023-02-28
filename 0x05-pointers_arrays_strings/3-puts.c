bc4b7e0be2cd4d5958496801b1fbf2b2a51c71b9
seh sekou <sehsekou98@gmail.com>
2023-02-28 08:53:35 -0800

    3-put.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put - prints a string, followed by a new line, to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 
