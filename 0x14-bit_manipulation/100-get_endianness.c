49a80cd33c94a1cedf05b226352f5bf9f5532df8
sehsekou98 <sehsekou98@gmail.c>
2023-04-11 11:35:05 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * - checks if a machine is little endian or big endian
+ *   RETURN: 0 for big and 1 for little
+ */
+
+int get_endianness(void)
+{
+	unsigned int sum = 1;
+
+	char *ptrc = (char *) &sum;
+
+	return(*ptrc);
+}
+
