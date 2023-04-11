a5d5f8a7bb731e3cf4a3fd42f5e43517805c36f7
sehsekou98 <sehsekou98@gmail.c>
2023-04-10 18:34:56 -0700

    1-print_binary

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/*
+ * _putchat - write the charter c
+ * @c: the character to print
+ * RETURN: on success 1
+ * on error of, -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
